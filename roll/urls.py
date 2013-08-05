bf899c353440c5c1abffdc4c6ce891b7792419c6
Panos Louridas <louridas@gmail.com>
2013-08-05 16:54:42 +0300

    better thanks

--- roll/urls.py
+++ roll/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls import patterns, url
 from roll import views
 
 urlpatterns = patterns('',
-                       url(r'^registration/(?P<unique_id>.+)/thanks',
-                           views.register_thanks,
-                           name='register_thanks'),                       
                        url(r'^registration/(?P<unique_id>.+)',
                            views.register,
                            name='register'),
