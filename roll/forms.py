b622b20713b9ca015fe6db4a894d2cd7c2f6efee
Panos Louridas <louridas@gmail.com>
2013-08-05 11:57:19 +0300

    work on registration form

--- roll/forms.py
+++ roll/forms.py
@@ -3,14 +3,23 @@ from django import forms
 from models import * 
 
 class RegistrationForm(forms.ModelForm):
-    
+    voter_name = forms.CharField(max_length=200)
+    voter_email = forms.EmailField()
+    voter_mobile_phone = forms.CharField(max_length=20)
+
+    voter_field_names = ('voter_name', 'voter_email', 'voter_mobile_phone',)
+
     def __init__(self, *args, **kwargs):        
         super(RegistrationForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.iteritems():
             field.widget.attrs = {
                 'class': 'form-control',
-                'readonly': 'readonly'
-            }        
+            }
+            if not field_name.startswith('voter'):
+                field.widget.attrs['readonly'] = 'true'
+
+        if not self.instance:
+            return
         self.fields['location'].queryset = Location.objects.filter(
             id=self.instance.location_id)
         self.fields['rating'].queryset = Rating.objects.filter(
@@ -20,9 +29,18 @@ class RegistrationForm(forms.ModelForm):
         self.fields['owner'].queryset = Owner.objects.filter(
             id=self.instance.owner_id)
         self.fields['electoral_group'].queryset = ElectoralGroup.objects.filter(
-            id=self.instance.electoral_group_id)        
+            id=self.instance.electoral_group_id)
 
+    def voter_fields(self):
+        return [field for field in self.visible_fields()
+                if field.name in RegistrationForm.voter_field_names ]
+
+    def non_voter_fields(self):
+        return [field for field in self.visible_fields()
+                if field.name not in RegistrationForm.voter_field_names ]
+        
     class Meta:
         model = Establishment
-        exclude = ('license', 'unique_id', 'fee_payment',)
+        exclude = ('license', 'unique_id', 'fee_payment', 'voter',)
 
+    
--- roll/views.py
+++ roll/views.py
@@ -2,30 +2,48 @@ from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
-from models import Establishment
+from models import Establishment, Voter
 
 from forms import RegistrationForm
 
 def register(request, unique_id=None):
     establishment = None
+    voter = None
     if unique_id:
         establishment = Establishment.objects.get(unique_id=unique_id)
+        if establishment.voter_id:
+            voter = establishment.voter
+        else:
+            voter = Voter()
     if request.method == 'POST':
-        if establishment_id:
+        if unique_id:
             form = RegistrationForm(request.POST, instance=establishment)
         else:
             form = RegistrationForm(request.POST)
         if form.is_valid():
-            establishment = form.save()
-            return redirect('/roll/registration-thanks')
+            voter.name = request.POST.get('voter_name', '')
+            voter.email = request.POST.get('voter_email', '')
+            voter.mobile_phone = request.POST.get('voter_mobile_phone', '')
+            voter.save()
+            establishment.voter = voter
+            establishment.save()
+            return redirect(request.path + '/thanks')
     else:
         if unique_id:
-            form = RegistrationForm(instance=establishment)
+            form = RegistrationForm(
+                instance=establishment,
+                initial={
+                    'voter_name': voter.name,
+                    'voter_email': voter.email,
+                    'voter_mobile_phone': voter.mobile_phone,
+                })
         else:
             form = RegistrationForm()
     return render(request, 'roll/registration.html', {
         'form': form,
+        'voter': voter,
+        'form_action': request.path,
     })
 
-def register_thanks(request, establishment_id=None):
+def register_thanks(request, unique_id=None):
         return render(request, 'roll/thanks.html')
