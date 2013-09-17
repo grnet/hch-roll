2679866c51aa6cc494f245e0be0ccf097824b8ab
Panos Louridas <louridas@gmail.com>
2013-09-17 21:04:47 +0300

    add voter extraction functionality

---
+++ roll/management/commands/extract_voters.py
@@ -0,0 +1,116 @@
+ #!/usr/bin/python
+ # -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist
+
+from roll.models import Establishment
+
+from collections import defaultdict
+
+import sys
+import csv
+
+VOTER_MAPPING = {
+    (1, '5*****'): 1,
+    (1, '4****'): 1,
+    (1, '3***'): 1,
+    (1, '2**'): 1,
+    (1, '1*'): 1,
+    (2, '5*****'): 2,
+    (2, '4****'): 2,
+    (2, '3***'): 2,
+    (2, '2**'): 3,
+    (2, '1*'): 3,
+    (3, '5*****'): 4,
+    (3, '4****'): 4,
+    (3, '3***'): 4,
+    (3, '2**'): 5,
+    (3, '1*'): 5,
+    (4, '5*****'): 6,
+    (4, '4****'): 7,
+    (4, '3***'): 8,
+    (4, '2**'): 9,
+    (4, '1*'): 10,
+    (5, '5*****'): 11,
+    (5, '4****'): 12,
+    (5, '3***'): 13,
+    (5, '2**'): 14,
+    (5, '1*'): 15,
+    (6, '5*****'): 16,
+    (6, '4****'): 16,
+    (6, '3***'): 16,
+    (6, '2**'): 17,
+    (6, '1*'): 17, 
+    (7, '5*****'): 18,
+    (7, '4****'): 19,
+    (7, '3***'): 20,
+    (7, '2**'): 21,
+    (7, '1*'): 22,
+    (8, '5*****'): 23,
+    (8, '4****'): 24,
+    (8, '3***'): 25,
+    (8, '2**'): 26,
+    (8, '1*'): 27,
+    (9, '5*****'): 28,
+    (9, '4****'): 28,
+    (9, '3***'): 28,
+    (9, '2**'): 28,
+    (9, '1*'): 28,
+    (10, '5*****'): 29,
+    (10, '4****'): 29,
+    (10, '3***'): 29,
+    (10, '2**'): 29,
+    (10, '1*'): 29,
+    (11, '5*****'): 30,
+    (11, '4****'): 30,
+    (11, '3***'): 30,
+    (11, '2**'): 30,
+    (11, '1*'): 30,
+    (12, u"Α'ΤΑΞΗΣ"): 31,
+    (12, u"Β'ΤΑΞΗΣ"): 31,
+    (12, u"Γ'ΤΑΞΗΣ"): 31,
+    (12, u"Δ'ΤΑΞΗΣ"): 31,
+    (12, u"Ε'ΤΑΞΗΣ"): 31,
+}
+
+VOTER_BALLOTS = defaultdict(list)
+
+class Command(BaseCommand):
+    help = """Extracts voters files"""
+        
+    def list_voters(self, args, options):
+        establishments = Establishment.objects.select_related('voter',
+                                                              'rating').filter(
+            voter_id__isnull=False)
+                                                              
+        for establishment in establishments:
+            row = [
+                establishment.registry_number,
+                establishment.voter.email,
+                establishment.voter.first_name,
+                establishment.voter.surname,
+                establishment.name,
+                establishment.voter.mobile_phone
+            ]
+            urow = [c.encode('utf-8') if isinstance(c, basestring) else c
+                    for c in row ]
+            rating = establishment.rating.name
+            key = (establishment.electoral_group.code, rating)
+            if key not in VOTER_MAPPING:
+                self.stdout.write(u"{0} {1} {2}".format(key[0], key[1],
+                                                        establishment))
+            else:
+                ballot = VOTER_MAPPING[key]
+                VOTER_BALLOTS[ballot].append(urow)
+        for ballot in sorted(VOTER_BALLOTS.keys()):
+            with open("ballot_{0}.csv ".format(ballot), 'wb') as ballot_file:
+                ballot_writer = csv.writer(ballot_file)
+                for voter in VOTER_BALLOTS[ballot]:
+                    ballot_writer.writerow(voter)
+
+    def handle(self, *args, **options):
+        self.list_voters(args, options)
+
+
+
---
+++ roll/management/commands/update_roll.py
@@ -0,0 +1,128 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from roll.models import *
+
+from collections import namedtuple
+from datetime import datetime
+import csv
+import django
+import re
+
+from functools import partial
+
+from django.db import transaction
+
+ROW_HEADER = [
+    'registry_number',
+    'name',
+    'location',
+    'establishment_type',
+    'rating',
+    'operator',
+    'owner',
+    'license',
+    'fee_paid',
+    'payment_date',
+    'telephone',
+    'fax',
+    'email',
+    'electoral_group_code',
+    'electoral_group_name',
+    'prefecture',
+    'region',
+    'island',
+    'street_number',
+    'zip_code',
+    'city',
+    'voter_first_name',
+    'voter_surname',
+    'voter_email',
+    'voter_mobile_phone',
+]
+
+class Command(BaseCommand):
+    args = '<update_data_csv_file>'
+    help = 'Updates the database'
+    
+    @transaction.commit_on_success
+    def update_roll(self, datafile):
+        if django.get_version() >= '1.5':
+            write = partial(self.stdout.write, ending = '')
+        else:
+            write = self.stdout.write
+        RollRow = namedtuple('RollRow', ROW_HEADER)
+        for line, row in enumerate(
+                map(lambda r: RollRow._make([s.decode('utf-8') for s in r]),
+                    csv.reader(open(datafile, 'rb')))):
+            registry_number = row.registry_number
+            name = row.name
+            location, c = Location.objects.get_or_create(name=row.location)
+            establishment_type, c = EstablishmentType.objects.get_or_create(
+                name=row.establishment_type)
+            rating, c = Rating.objects.get_or_create(name=row.rating,
+                                                     position=0)
+            operator, c = Operator.objects.get_or_create(name=row.operator)
+            owner, c = Owner.objects.get_or_create(name=row.owner)
+            license = row.license
+            if len(row.payment_date) > 0:
+                payment_date = datetime.strptime(row.payment_date, '%Y-%m-%d')
+                fee_payment, c = FeePayment.objects.get_or_create(
+                    fee_paid=row.fee_paid,
+                    payment_date=payment_date)
+            else :
+                fee_payment, c = FeePayment.objects.get_or_create(
+                    fee_paid=row.fee_paid)
+            telephone = row.telephone
+            fax = row.fax
+            email = row.email
+            electoral_group, c = ElectoralGroup.objects.get_or_create(
+                code=row.electoral_group_code,
+                name=row.electoral_group_name)
+            region, c = Region.objects.get_or_create(name=row.region)
+            prefecture, c = Prefecture.objects.get_or_create(
+                name=row.prefecture,
+                region=region)
+            city, c = City.objects.get_or_create(name=row.city,
+                                                 prefecture=prefecture)
+            island, c = Island.objects.get_or_create(name=row.island)
+            address, c = Address.objects.get_or_create(
+                street_number=row.street_number,
+                zip_code=row.zip_code,
+                city=city,
+                island=island,
+                location=location)
+            establishment, c = Establishment.objects.get_or_create(
+                registry_number=registry_number)
+            establishment.name = name
+            establishment.establishment__type = establishment_type
+            establishment.address = address
+            establishment.rating = rating
+            establishment.operator = operator
+            establishment.owner = owner
+            establishment.license = license
+            establishment.fee_payment = fee_payment
+            establishment.telephone = telephone
+            establishment.fax = fax
+            establishment.email = email
+            establishment.electoral_group = electoral_group
+            if c:
+                establishment.unique_id = Establishment.generate_unique_id()
+            if establishment.voter_id is None:
+                voter = Voter(first_name = row.voter_first_name,
+                              surname=row.voter_surname,
+                              email=row.voter_email,
+                              mobile_phone=re.sub("[^0-9]", "",
+                                                  row.voter_mobile_phone))
+                voter.save()
+                establishment.voter = voter
+            establishment.save()
+            write("\r{0}".format(line+1))
+            self.stdout.flush()            
+        write("\n")
+
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            raise CommandError("No input data file")
+        else:
+            self.update_roll(args[0])
+
