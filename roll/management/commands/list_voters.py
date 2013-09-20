5eb9f40184982c6849135a5740775bf1d92ab83d
Panos Louridas <louridas@gmail.com>
2013-09-20 16:49:49 +0300

    better algorithm, ordered output

--- roll/management/commands/list_voters.py
+++ roll/management/commands/list_voters.py
@@ -4,7 +4,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
 
-from roll.models import Voter
+from roll.models import Establishment
 
 import sys
 import csv
@@ -41,42 +41,41 @@ class Command(BaseCommand):
     help = """Shows list of voters"""
         
     def list_voters(self, args, options):
-        voters = Voter.objects.all()
+        establishments = Establishment.objects.select_related().filter(
+            voter_id__isnull=False).order_by('registry_number').all()
         voter_writer = csv.writer(sys.stdout)
         voter_writer.writerow([h.encode('utf-8') for h in ROW_HEADER])
-        for voter in voters:
-            for establishment in voter.establishment_set.select_related().all():
-                row = [
-                    establishment.registry_number,
-                    establishment.name,
-                    establishment.address.location.name,
-                    establishment.establishment_type.name,
-                    establishment.rating.name,
-                    establishment.operator.name,
-                    establishment.owner.name,
-                    establishment.license,
-                    establishment.fee_payment.fee_paid,
-                    establishment.fee_payment.payment_date,
-                    establishment.telephone,
-                    establishment.fax,
-                    establishment.email,
-                    establishment.electoral_group.code,
-                    establishment.electoral_group.name,
-                    establishment.address.city.prefecture.name,
-                    establishment.address.city.prefecture.region.name,
-                    establishment.address.island,
-                    establishment.address.street_number,
-                    establishment.address.zip_code,
-                    establishment.address.city,
-                    voter.first_name,
-                    voter.surname,
-                    voter.email,
-                    voter.mobile_phone
-                ]
-                urow = [c.encode('utf-8') if isinstance(c, basestring) else c
-                        for c in row ]
-                voter_writer.writerow(urow)
-        
+        for establishment in establishments:
+            row = [
+                establishment.registry_number,
+                establishment.name,
+                establishment.address.location.name,
+                establishment.establishment_type.name,
+                establishment.rating.name,
+                establishment.operator.name,
+                establishment.owner.name,
+                establishment.license,
+                establishment.fee_payment.fee_paid,
+                establishment.fee_payment.payment_date,
+                establishment.telephone,
+                establishment.fax,
+                establishment.email,
+                establishment.electoral_group.code,
+                establishment.electoral_group.name,
+                establishment.address.city.prefecture.name,
+                establishment.address.city.prefecture.region.name,
+                establishment.address.island,
+                establishment.address.street_number,
+                establishment.address.zip_code,
+                establishment.address.city,
+                establishment.voter.first_name,
+                establishment.voter.surname,
+                establishment.voter.email,
+                establishment.voter.mobile_phone
+            ]
+            urow = [c.encode('utf-8') if isinstance(c, basestring) else c
+                    for c in row ]
+            voter_writer.writerow(urow)
 
     def handle(self, *args, **options):
         self.list_voters(args, options)
